96e34f6fb66ff5cbede029e8371c0b96a9cb2180
suisho <suisho000000@gmail.com>
2014-05-12 00:01:24 +0900

    frame

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Content script');
+
+var listItem = $(".hoverable li")
+
+var initialItem = function($item){
+  var $gif = $item.find("a.gif")
+  var data = $gif.data("id")
+  var item = $("<a>") //TODO
+  item.on('click', function(){
+    $.post("/favorites/" + data, function(){
+      // TODO
+    })
+  })
+}
