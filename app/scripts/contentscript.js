dccf9767a870ed13d96d4b7cb858c7b134dd2d23
suisho <suisho000000@gmail.com>
2014-05-15 00:03:34 +0900

    update

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,16 +1,35 @@
 'use strict';
-
-console.log('\'Allo \'Allo! Content script');
-
-var listItem = $(".hoverable li")
-
-var initialItem = function($item){
-  var $gif = $item.find("a.gif")
-  var data = $gif.data("id")
-  var item = $("<a>") //TODO
-  item.on('click', function(){
-    $.post("/favorites/" + data, function(){
-      // TODO
+document.onreadystatechange = function () {
+  var execute = function(){
+    var listItem = $(".hoverable li")
+    listItem.forEach(function(item){
+      initialItem(item)
     })
-  })
+
+
+  }
+
+  var initialItem = function($item){
+    var $gif = $item.find("a.gif")
+    var data = $gif.data("id")
+    var $item = createFavoriteLink()
+    $gif.append($item)
+    /*$item.on('click', function(){
+      $.post("/favorites/" + data, function(){
+      })
+    })*/
+  }
+  // create a.class div.facorite-icon
+  var createFavoriteLink = function(){
+    var $div = $("<div>").addClass("favorite-icon")
+    var $a = $("<a>").addClass("favorite")
+    $a.append($div)
+    return $a
+  }
+  
+  // endpoint
+  if (document.readyState == "complete") {
+    execute();
+  }
+
 }
